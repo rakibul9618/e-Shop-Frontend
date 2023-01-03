848661730ee1cc524875057d4c3137ad8c56724c
rakib <rhkhan9618@gmail.com>
2023-01-03 21:40:59 +0600

    test change

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -55,6 +55,24 @@ const accordionList = [
                 "title": "Sun Care",
             },
         ]
+    },
+     {
+        "id": 4,
+        "title": "Category",
+        "items": [
+            {
+                "id": 1,
+                "title": "Sun Care",
+            },
+            {
+                "id": 2,
+                "title": "Sun Care",
+            },
+            {
+                "id": 3,
+                "title": "Sun Care",
+            },
+        ]
     }
 ]
 
