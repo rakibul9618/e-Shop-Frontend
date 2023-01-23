b3515222c55a38d163750c3d844671381af6a02f
rakib <rhkhan9618@gmail.com>
2023-01-23 22:18:04 +0600

    build: check commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,24 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'docs',
+        // feat refers to new features
+        'feat',
+        'fix',
+        // perf refers to A code change that improves performance
+        'perf',
+        // refactor refers to  A code change that neither fixes a bug nor adds a feature
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
-
-// rules: {
-//   'type-enum': [
-//     2,
-//     'always',
-//     [
-//       'build',
-//       'chore',
-//       'docs',
-//       'feat',
-//       'fix',
-//       'perf',
-//       'refactor',
-//       'revert',
-//       'style',
-//       'test',
-//     ],
-//   ],
-// },
